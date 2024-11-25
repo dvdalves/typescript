3ad1c2671f55618b9629366e76eeec980d336375
David Alves <david.a0595@gmail.com>
2024-11-25 12:54:19 -0300

    first commit

---
+++ cap1/cap1.js
@@ -0,0 +1,5 @@
+// Iniciando o livro de Typescript, começando com um código simples em Javascript puro.
+
+const firstName = "Georgia";
+const nameLength = firstName.length;
+console.log(nameLength);
